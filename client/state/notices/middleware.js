67ecdd3fffc07c7c8a67a11ce58e4b7c16efe589
Andrew Duthie <andrew@andrewduthie.com>
2016-07-11 10:01:09 -0400

    State: Move post save notice handlers to middleware

--- client/state/notices/middleware.js
+++ client/state/notices/middleware.js
@@ -1,4 +1,32 @@
-export const OBSERVERS = {};
+/**
+ * External dependencies
+ */
+import { translate } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import { successNotice } from 'state/notices/actions';
+import { POST_SAVE_SUCCESS } from 'state/action-types';
+
+export const OBSERVERS = {
+	[ POST_SAVE_SUCCESS ]: ( action, dispatch ) => {
+		let text;
+		switch ( action.post.status ) {
+			case 'trash':
+				text = translate( 'Post successfully moved to trash' );
+				break;
+
+			case 'publish':
+				text = translate( 'Post successfully published' );
+				break;
+		}
+
+		if ( text ) {
+			dispatch( successNotice( text ) );
+		}
+	}
+};
 
 export default function noticesMiddleware( { dispatch, getState } ) {
 	return ( next ) => ( action ) => {
--- client/state/notices/test/middleware.js
+++ client/state/notices/test/middleware.js
@@ -11,35 +11,85 @@ import { noop } from 'lodash';
 import noticesMiddleware, { OBSERVERS } from '../middleware';
 import { useSandbox } from 'test/helpers/use-sinon';
 import { successNotice } from 'state/notices/actions';
-import { NOTICE_CREATE } from 'state/action-types';
+import { NOTICE_CREATE, POST_SAVE_SUCCESS } from 'state/action-types';
 
 describe( 'middleware', () => {
-	function dummyNoticeObserver( action, dispatch, getState ) {
-		dispatch( successNotice( `${ getState() } ${ action.target }` ) );
-	}
-
 	let store;
 	useSandbox( ( sandbox ) => {
 		store = createStore( () => 'Hello' );
 		sandbox.spy( store, 'dispatch' );
 	} );
 
-	before( () => {
-		OBSERVERS.DUMMY_TYPE = dummyNoticeObserver;
+	describe( 'noticesMiddleware()', () => {
+		function dummyNoticeObserver( action, dispatch, getState ) {
+			dispatch( successNotice( `${ getState() } ${ action.target }` ) );
+		}
+
+		before( () => {
+			OBSERVERS.DUMMY_TYPE = dummyNoticeObserver;
+		} );
+
+		after( () => {
+			delete OBSERVERS.DUMMY_TYPE;
+		} );
+
+		it( 'should trigger the observer corresponding to the dispatched action type', () => {
+			noticesMiddleware( store )( noop )( { type: 'DUMMY_TYPE', target: 'World' } );
+
+			expect( store.dispatch ).to.have.been.calledWithMatch( {
+				type: NOTICE_CREATE,
+				notice: {
+					text: 'Hello World'
+				}
+			} );
+		} );
 	} );
 
-	after( () => {
-		delete OBSERVERS.DUMMY_TYPE;
-	} );
+	describe( 'OBSERVERS', () => {
+		context( '.POST_SAVE_SUCCESS', () => {
+			it( 'should not dispatch if status has no corresponding text', () => {
+				noticesMiddleware( store )( noop )( {
+					type: POST_SAVE_SUCCESS,
+					post: {
+						title: 'Hello World',
+						status: 'invalid'
+					}
+				} );
 
-	it( 'should trigger the observer corresponding to the dispatched action type', () => {
-		noticesMiddleware( store )( noop )( { type: 'DUMMY_TYPE', target: 'World' } );
+				expect( store.dispatch ).to.not.have.been.calledWithMatch( {
+					type: NOTICE_CREATE
+				} );
+			} );
 
-		expect( store.dispatch ).to.have.been.calledWithMatch( {
-			type: NOTICE_CREATE,
-			notice: {
-				text: 'Hello World'
-			}
+			it( 'should dispatch success notice for trash', () => {
+				noticesMiddleware( store )( noop )( {
+					type: POST_SAVE_SUCCESS,
+					post: { status: 'trash' }
+				} );
+
+				expect( store.dispatch ).to.have.been.calledWithMatch( {
+					type: NOTICE_CREATE,
+					notice: {
+						status: 'is-success',
+						text: 'Post successfully moved to trash'
+					}
+				} );
+			} );
+
+			it( 'should dispatch success notice for publish', () => {
+				noticesMiddleware( store )( noop )( {
+					type: POST_SAVE_SUCCESS,
+					post: { status: 'publish' }
+				} );
+
+				expect( store.dispatch ).to.have.been.calledWithMatch( {
+					type: NOTICE_CREATE,
+					notice: {
+						status: 'is-success',
+						text: 'Post successfully published'
+					}
+				} );
+			} );
 		} );
 	} );
 } );
